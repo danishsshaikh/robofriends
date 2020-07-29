49b6da7f011b89a024d4c2e27a3d2b671ea69687
danishsshaikh <danishsshaikh@gmail.com>
2020-07-29 17:56:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
-ReactDOM.render(
-  <React.StrictMode>
-  	<CardList robots={robots}/>
-  </React.StrictMode>,
+ReactDOM.render(<App />	,
   document.getElementById('root')
 );
 
